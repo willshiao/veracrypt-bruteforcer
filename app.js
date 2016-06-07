08b21629fd026b0962cb693549889220b6b0318e
William Shiao <willshiao@gmail.com>
2016-06-06 22:05:08 -0700

    feat: Add main code

---
+++ app.js
@@ -0,0 +1,122 @@
+//********************
+// Dependencies
+//********************
+const Promise = require('bluebird');
+const _ = require('lodash');
+const drivelist = Promise.promisifyAll(require('drivelist'));
+const ps = Promise.promisifyAll(require('ps-node'));
+const deasync = require('deasync');
+const exec = require('child_process').exec;
+const execSync = deasync(require('child_process').exec);
+const fs = Promise.promisifyAll(require('fs'));
+const argv = require('minimist')(process.argv.slice(2), {
+  alias: {
+    wordlist: 'w',
+    volume: 'v',
+    letter: ['l', 'mount', 'm'],
+    path: 'p'
+  },
+  default: {
+    path: '"D:\\Program Files\\VeraCrypt\\VeraCrypt.exe"'
+  }
+});
+
+const veracryptRunningAsync = Promise.promisify(veracryptRunning);
+const veracryptRunningSync = deasync(veracryptRunning);
+const diskMountedSync = deasync(diskMounted);
+
+//********************
+// Main
+//********************
+
+var hasError = false;
+var wordlist;
+var ready; //is the program ready to try a new password?
+
+//Check arguments
+if(!argv.wordlist) {
+  console.error('Missing wordlist argument');
+  hasError = true;
+}
+if(!argv.volume) {
+  console.error('Missing volume argument');
+  hasError = true;
+}
+if(!argv.letter) {
+  console.error('Missing drive letter argument');
+  hasError = true;
+}
+if(hasError) process.exit(2);
+
+veracryptRunningAsync().then(running => {
+  if(running) {
+    exitWithMessage('Please close VeraCrypt before running this program.', 3);
+  }
+  return Promise.fromCallback(cb => diskMounted(argv.letter, cb));
+}).then(mounted => {
+  if(mounted) {
+    exitWithMessage('Drive letter ('+argv.letter+') has already been mounted. '+
+      'Please unmount or choose a different drive letter', 4);
+  }
+  return fs.readFileAsync(argv.wordlist, 'utf8');
+}).then(wordlistStr => {
+  wordlist = wordlistStr.split(/\r?\n/);
+  for(var i = 0; i < wordlist.length; i++) {
+    if(testPassword(wordlist[i], argv)) {
+      console.log('Found password:', wordlist[i]);
+      process.exit(0);
+    }
+  }
+  console.log('Password not found.');
+  process.exit(0);
+}).catch(err => {
+  console.error('An error occurred:', err.stack);
+  process.exit(5);
+});
+
+//********************
+// Functions
+//********************
+
+function testPassword(password, args) {
+  console.log('Trying:', password);
+  const query = 'START "" '+args.path+' /v "'+args.volume+'" /q /p "'+password+'" /s /l '+args.letter;
+  // console.log('Query:', query);
+  execSync(query);
+  deasync.loopWhile(() => veracryptRunningSync());
+  console.log(args.letter, ' mounted?:', diskMountedSync(args.letter));
+  if(diskMountedSync(args.letter))
+    return true;
+  else return false;
+}
+
+function exitWithMessage(msg, code) {
+  console.error(msg);
+  process.exit(code);
+}
+
+function veracryptRunning(cb) {
+  console.log('Querying process...');
+  exec('query process VeraCrypt.exe', (err, stdout, stderr) => {
+    //Check for errors, but only if the error code is not 1
+    //since the default error code for the command is 1
+    if(err && err.code !== 1) return cb(err);
+    cb(null, (stderr.indexOf('No Process exists') === -1));
+  });
+}
+
+function diskMounted(letter, cb) {
+  return drivelist.listAsync().then(drives => {
+    for(var i = 0; i < drives.length; i++) {
+      if(drives[i].mountpoint === (letter+':')) {
+        console.log('Drive mounted!');
+        return Promise.resolve(true);
+      }
+    }
+    return Promise.resolve(false);
+  }).then(res => {
+    cb(null, res);
+  }).catch(err => {
+    cb(err);
+  });
+}
